103a6e8fd7aff782c4c257b7656f177bc826e075
ekanadya <ekanadya96@gmail.com>
2020-04-23 20:10:12 +0800

    tambah sintaks koneksi login agen ke database

--- login-agen.php
+++ login-agen.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,12 +8,43 @@
 </head>
 <body>
     <h3>Login Agen</h3>
+    <?php if(isset($error)) : ?>
+        <p>email atau password salah</p>
+    <?php endif; ?>
+
+
     <p>
         <ul>
-            <li>Email : <input type="text" id="email"></li>
-            <li>Password : <input type="text" id="password"></li>
+            <li>Email : <input type="text" id="email" name="email"></li>
+            <li>Password : <input type="password" id="password" name="password"></li>
             <li><input type="submit" name="login">Login</input> <a href="/lupa-kata-sandi.php">Lupa Kata Sandi ?</a></li>
         </ul>
     </p>
 </body>
 </html>
+
+<?php
+ 
+ session_start();
+ include 'connect-db.php'
+
+if ( isset($_POST["login"]) ){
+     $email = $_POST["email"];
+     $password = $_POST["password"];
+
+    $result = mysqli_query($connect, "SELECT * FROM agen WHERE email = '$email'");
+
+    if(mysqli_num_rows($result) == 1){
+        $row = mysqli_fetch_assoc($result);
+        if(password_verify($password, $row["password"])){
+            header("Location: index.php");
+            exit;
+        }
+    }
+
+    $error = true;
+ }
+    
+?>
+
+
