c442147ac1e338d71e86eefa577a16a665dde397
ekanadya <ekanadya96@gmail.com>
2020-04-24 09:58:36 +0800

    buat registrasi-agen2

---
+++ registrasi-agen2.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrasi Agen</title>
+</head>
+<body>
+    <div id="header">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href='registrasi.php'>Registrasi</a></li>
+            <li><a href='login.php'>Login</a></li>
+        </ul>
+    </div>
+    <div id="body">
+        <div class="body">
+            <div><img src="files/laundryku.jpg" alt="laundryku" width=300 height=auto/></div>
+            <div><b>Syarat dan Ketentuan :</b></div>
+            <div>1. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Non, accusamus? Excepturi officia inventore dolor, quisquam facere ipsum quis perspiciatis. Consequuntur rem molestiae sint, commodi atque magnam. Unde blanditiis quam quo.</div>
+            <div>2. Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic natus aut rerum similique ad, voluptatibus magnam tenetur velit sapiente dicta sunt molestiae culpa deleniti, corrupti dolor unde, beatae ea eos.</div>
+            <div>3. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deserunt hic laboriosam beatae explicabo, et consequatur? Omnis error sapiente accusamus soluta cum minus libero quasi ab ut, quo rerum hic aspernatur?</div>
+            <div>4. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente iusto eligendi ex odio quia reiciendis in expedita eveniet dicta tempore, maxime, laboriosam hic nostrum inventore assumenda accusantium perferendis illo voluptate!</div>
+            <div>5. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, eveniet suscipit repellendus non dolore repellat! At, reprehenderit tempora! Accusamus ut itaque veritatis doloremque delectus dolorem architecto quo perspiciatis reiciendis unde?</div>
+        </div>
+        <div class="body">
+            <h3>Lengkapi Profil</h3>
+            <form action="" method="post">
+                <ul>
+                    <li><a href="#">Cuci</a><span class="panah">&#9660</span>
+                    <ul class="sub1">
+                        <li><a href="#">Satu</a></li>
+                        <li><a href="#">Dua</a></li>
+                        <li><a href="#">Tiga</a></li>
+                    </ul></li>
+                    <li><input type="text" name="namaLaundry" placeholder="Harga"></li>
+                    <li><a href="#">Cuci</a><span class="panah">&#9660</span>
+                    <ul class="sub1">
+                        <li><a href="#">Satu</a></li>
+                        <li><a href="#">Dua</a></li>
+                        <li><a href="#">Tiga</a></li>
+                    </ul></li>
+                    <li><input type="text" name="namaLaundry" placeholder="Harga"></li>
+                    <li><a href="#">Cuci</a><span class="panah">&#9660</span>
+                    <ul class="sub1">
+                        <li><a href="#">Satu</a></li>
+                        <li><a href="#">Dua</a></li>
+                        <li><a href="#">Tiga</a></li>
+                    </ul></li>
+                    <li><input type="text" name="namaLaundry" placeholder="Harga"></li>
+                    <li><button type="submit" name="daftar">Lanjutkan</button></li>
+                </ul>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
