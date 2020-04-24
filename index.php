23b550f77d7174d214fa239c6c842cd36d6d364c
Hairul Lana <hairullana99@gmail.com>
2020-04-25 00:29:44 +0800

    buat pagination dan searching di index

--- index.php
+++ index.php
@@ -1,7 +1,42 @@
 <?php
 
 session_start();
-include 'connect-db.php'
+include 'connect-db.php';
+
+
+//konfirgurasi pagination
+$jumlahDataPerHalaman = 3;
+$query = mysqli_query($connect,"SELECT * FROM agen");
+$jumlahData = mysqli_num_rows($query);
+//ceil() = pembulatan ke atas
+$jumlahHalaman = ceil($jumlahData / $jumlahDataPerHalaman);
+
+//menentukan halaman aktif
+//$halamanAktif = ( isset($_GET["page"]) ) ? $_GET["page"] : 1; = versi simple
+if ( isset($_GET["page"])){
+    $halamanAktif = $_GET["page"];
+}else{
+    $halamanAktif = 1;
+}
+
+//data awal
+$awalData = ( $jumlahDataPerHalaman * $halamanAktif ) - $jumlahDataPerHalaman;
+
+//fungsi memasukkan data di db ke array
+$agen = mysqli_query($connect,"SELECT * FROM agen LIMIT $awalData, $jumlahDataPerHalaman");
+
+//ketika tombol cari ditekan
+if ( isset($_POST["cari"])) {
+    $keyword = $_POST["keyword"];
+
+    $query = "SELECT * FROM agen WHERE 
+        kota LIKE '%$keyword%' OR
+        nama_laundry LIKE '%$keyword%'
+        LIMIT $awalData, $jumlahDataPerHalaman
+        ";
+
+    $agen = mysqli_query($connect,$query);
+}
 
 ?>
 
@@ -48,7 +83,7 @@ include 'connect-db.php'
                         ";
                     }else if ( isset($_SESSION["login-admin"]) && isset($_SESSION["admin"])){
                         echo "
-                            <a href'admin.php'>Admin</a>
+                            <a href='admin.php'>Admin</a>
                         ";
                     }else {
                         echo "
@@ -114,27 +149,42 @@ include 'connect-db.php'
                 </ul>
             <?php else : ?>
                 <a href='registrasi-pelanggan.php'><button type='button'>Daftar Sekarang</button></a>
+                <form action="" method="post">
+                    <input type="text" name="keyword" placeholder="Kota / Kabupaten" />
+                    <button type="submit" name="cari">CARI</button>
+                </form>
             <?php endif ?>
         </div>
         </div>
         <div class="body">
-            <?php
-                // ambil data di database
-                $agen = mysqli_query($connect, "SELECT * FROM agen");
+            <div id="halaman">
+                <?php if( $halamanAktif > 1 ) : ?>
+                    <a href="?page=<?= $halamanAktif - 1; ?>">&laquo;</a>
+                <?php endif; ?>
 
-                // meampilkan agen
-                foreach ( $agen as $dataAgen) :
-            ?>
-            <div style="margin-top:20px" class="agen">
-                <div><img src="files/laundryku.jpg" width=100 height=100 alt="foto"></div>
-                <h3><?= $dataAgen["nama_laundry"] ?></h3>
-                <div>
-                    <ul>
-                        <li>Alamat : <?= $dataAgen["alamat"] ?></li>
-                        <li>No. Telp : <?= $dataAgen["telp"] ?></li>
-                    </ul>
-                </div>
+                <?php for( $i = 1; $i <= $jumlahHalaman; $i++ ) : ?>
+                    <?php if( $i == $halamanAktif ) : ?>
+                        <a href="?page=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
+                    <?php else : ?>
+                        <a href="?page=<?= $i; ?>"><?= $i; ?></a>
+                    <?php endif; ?>
+                <?php endfor; ?>
+
+                <?php if( $halamanAktif < $jumlahHalaman ) : ?>
+                    <a href="?page=<?= $halamanAktif + 1; ?>">&raquo;</a>
+                <?php endif; ?>
             </div>
+            <?php foreach ( $agen as $dataAgen) : ?>
+                <div style="margin-top:20px" class="agen">
+                    <div><img src="files/laundryku.jpg" width=80 height=80 alt="foto" style="float:left;margin-right:10px"></div>
+                    <h3><?= $dataAgen["nama_laundry"] ?></h3>
+                    <div>
+                        <ul>
+                            <li>Alamat : <?= $dataAgen["alamat"] ?></li>
+                            <li>No. Telp : <?= $dataAgen["telp"] ?></li>
+                        </ul>
+                    </div>
+                </div>
             <?php endforeach; ?>
         </div>
     </div>
--- login-admin.php
+++ login-admin.php
@@ -17,23 +17,24 @@ if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"]) || iss
 
 // AKSI
 if ( isset($_POST["login"]) ) {
-    $email = $_POST["email"];
+    $username = $_POST["username"];
     $password = $_POST["password"];
 
     // cek di db
-    $data = mysqli_query($connect, "SELECT * FROM admin WHERE email = '$email'");
+    $data = mysqli_query($connect, "SELECT * FROM admin WHERE username = '$username'");
 
     // jika email ada
     if ( mysqli_num_rows($data) === 1 ){
 
         // jadikan array asosiatif
         $data = mysqli_fetch_assoc($data);
+        $idAdmin = $data["id_admin"];
 
         // jika password benar
         if ( $password === $data["password"])   {
             //session login 
             $_SESSION["login-admin"] = true;
-            $_SESSION["admin"] = $email;
+            $_SESSION["admin"] = $idAdmin;
 
             //jika remember me di checklist (NANTI DULU)
             // if ( isset($_POST["remember"]) ){
@@ -46,7 +47,7 @@ if ( isset($_POST["login"]) ) {
     }else {
         echo "
             <script>
-                alert ('Email Tidak Terdaftar');
+                alert ('Username Tidak Terdaftar');
                 document.location.href = 'login-admin.php';
             </script>
         ";
@@ -81,7 +82,7 @@ if ( isset($_POST["login"]) ) {
 
         <form action="" method="post">
             <ul>
-                <li>Email : <input type="text" name="email"></li>
+                <li>Email : <input type="text" name="username"></li>
                 <li>Password : <input type="text" name="password"></li>
                 <li><button type="submit" name="login">Login</button> <a href="/lupa-kata-sandi.php">Lupa Kata Sandi ?</a></li>
             </ul>
