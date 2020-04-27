8e681962cf09785b1606d593071ca02216a16288
nadyaoktvna <momoshi611@gmail.com>
2020-04-27 19:39:28 +0800

    header+index baru dikit - front end belum bisa nampilin daftar laundry dri database

--- header.php
+++ header.php
@@ -1,59 +1,102 @@
-<div id="header">
-    <ul>
-        <li><a href="index.php"><b>Home</b></a></li>
-        <li>
-            <?php
-                global $connect;
+<header>
+    <!-- Header Start -->
+       <div class="header-area">
+            <div class="main-header ">
+                <div class="header-bottom  header-sticky">
+                    <div class="container-fluid">
+                        <div class="row align-items-center">
+                            <!-- Logo -->
+                            <div class="col-xl-1 col-lg-1 col-md-1 col-sm-3">
+                                <div class="logo">
+                                  <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-8 col-md-7 col-sm-5">
+                                <!-- Main-menu -->
+                                <div class="main-menu f-left d-none d-lg-block">
+                                    <nav>                                                
+                                        <ul id="navigation">                                                                                                                                     
+                                            <li><a href="index.php">Home</a></li>
+                                        </ul>
+                                    </nav>
+                                </div>
+                            </div> 
+                            <div class="col-xl-5 col-lg-3 col-md-3 col-sm-3 fix-card">
+                                <ul class="header-right f-right d-none d-lg-block d-flex justify-content-between">
+                                    <li class="d-none d-xl-block">
+                                        <div class="form-box f-right ">
+                                            <form action="" method="POST">
+                                                <input type="text" name="Search" placeholder="Cari">
+                                                <div class="search-icon">
+                                                    <i class="fas fa-search special-tag"></i>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </li>
+                                    <li class="d-none d-lg-block">
+                                        <?php
+                                            if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"]) || isset($_SESSION["login-agen"]) && isset($_SESSION["agen"]) || isset($_SESSION["login-admin"]) && isset($_SESSION["admin"]) ){
+                                                echo "
+                                                    <a href='logout.php' class='btn header-btn'><b>LOGOUT</b></a>
+                                                ";
+                                            }else {
+                                                echo "
+                                                    <a href='login.php' class='btn header-btn'><b>LOGIN</b></a>
+                                                ";
+                                            }
+                                        ?>                                      
+                                    </li>
+                                    <li class="d-none d-lg-block"> 
+                                        <?php
+                                            global $connect;
 
-                if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"])){
-                    // mengambil email dari session
-                    $idPelanggan = $_SESSION["pelanggan"];
-                    // cari data di db sesuai $email
-                    $data = mysqli_query($connect, "SELECT * FROM pelanggan WHERE id_pelanggan = '$idPelanggan'");
-                    // memasukkan ke array asosiatif
-                    $data = mysqli_fetch_assoc($data);
-                    // mengambil data nama dari array
-                    $nama = $data["nama"];
+                                            if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"])){
+                                                // mengambil email dari session
+                                                $idPelanggan = $_SESSION["pelanggan"];
+                                                // cari data di db sesuai $email
+                                                $data = mysqli_query($connect, "SELECT * FROM pelanggan WHERE id_pelanggan = '$idPelanggan'");
+                                                // memasukkan ke array asosiatif
+                                                $data = mysqli_fetch_assoc($data);
+                                                // mengambil data nama dari array
+                                                $nama = $data["nama"];
 
-                    echo "
-                        <a href='pelanggan.php'><b>$nama</b></a> (Pelanggan)
-                    ";
-                }else if ( isset($_SESSION["login-agen"]) && isset($_SESSION["agen"])){
-                    // mengambil email dari session
-                    $id_agen = $_SESSION["agen"];
-                    // cari data di db sesuai $id_agen
-                    $data = mysqli_query($connect, "SELECT * FROM agen WHERE id_agen = '$id_agen'");
-                    // memasukkan ke array asosiatif
-                    $data = mysqli_fetch_assoc($data);
-                    // mengambil data nama dari array
-                    $nama = $data["nama_laundry"];
+                                                echo "
+                                                    <a href='pelanggan.php' class='btn header-btn'><b>$nama</b></a> (Pelanggan)
+                                                ";
+                                            }else if ( isset($_SESSION["login-agen"]) && isset($_SESSION["agen"])){
+                                                // mengambil email dari session
+                                                $id_agen = $_SESSION["agen"];
+                                                // cari data di db sesuai $id_agen
+                                                $data = mysqli_query($connect, "SELECT * FROM agen WHERE id_agen = '$id_agen'");
+                                                // memasukkan ke array asosiatif
+                                                $data = mysqli_fetch_assoc($data);
+                                                // mengambil data nama dari array
+                                                $nama = $data["nama_laundry"];
 
-                    echo "
-                        <a href='agen.php'><b>$nama</b></a> (Agen)
-                    ";
-                }else if ( isset($_SESSION["login-admin"]) && isset($_SESSION["admin"])){
-                    echo "
-                        <a href='admin.php'><b>Admin</b></a> (Admin)
-                    ";
-                }else {
-                    echo "
-                        <a href='registrasi.php'><b>Registrasi</b></a>
-                    ";
-                }
-            ?>
-        </li>
-        <li>
-            <?php
-                if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"]) || isset($_SESSION["login-agen"]) && isset($_SESSION["agen"]) || isset($_SESSION["login-admin"]) && isset($_SESSION["admin"]) ){
-                    echo "
-                        <a href='logout.php'><b>Logout</b></a>
-                    ";
-                }else {
-                    echo "
-                        <a href='login.php'><b>Login</b></a>
-                    ";
-                }
-            ?>
-        </li>
-    </ul>
-</div>
+                                                echo "
+                                                    <a href='agen.php'><b>$nama</b></a> (Agen)
+                                                ";
+                                            }else if ( isset($_SESSION["login-admin"]) && isset($_SESSION["admin"])){
+                                                echo "
+                                                    <a href='admin.php' class='btn header-btn'><b>ADMIN</b></a> (Admin)
+                                                ";
+                                            }else {
+                                                echo "
+                                                    <a href='registrasi.php' class='btn header-btn'><b>REGISTRASI</b></a>
+                                                ";
+                                            }
+                                        ?>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- Mobile Menu -->
+                            <div class="col-12">
+                                <div class="mobile_menu d-block d-lg-none"></div>
+                            </div>
+                        </div>
+                    </div>
+               </div>
+            </div>
+       </div>
+        <!-- Header End -->
+    </header>
--- index.php
+++ index.php
@@ -43,98 +43,161 @@ if ( isset($_POST["cari"])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="css/rating.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="assets/img/Mag1cwind0w-O-Sunny-Day-Osd-sun.ico">
+
+    <!-- CSS here -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/flaticon.css">
+        <link rel="stylesheet" href="assets/css/slicknav.css">
+        <link rel="stylesheet" href="assets/css/animate.min.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+        <link rel="stylesheet" href="assets/css/themify-icons.css">
+        <link rel="stylesheet" href="assets/css/slick.css">
+        <link rel="stylesheet" href="assets/css/nice-select.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/rating.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
     <title>Laundryku</title>
 </head>
 <body>
-    <?php include 'header.php'; ?>
-    <div id="body">
-        <div class="body">
-            <h1>LaundryKu</h1>
-            <h3>Solusi Laundry Praktis Tanpa Keluar Rumah</h3>
-            <?php if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"]) ) : ?>
-                <div>
-                    <form action="" method="post">
-                        <input type="text" name="keyword" placeholder="Kota / Kabupaten" />
-                        <button type="submit" name="cari">CARI</button>
-                    </form>
+    <!-- header -->
+        <?php include 'header.php'; ?>
+    <!--header end -->
+
+    <!-- slider Area Start -->
+    <div class="slider-area ">
+        <!-- Mobile Menu -->
+        <div class="slider-active">
+            <div class="container">
+                <div class="row d-flex align-items-center justify-content-between">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 d-none d-md-block">
+                        <div class="hero__img" data-animation="bounceIn" data-delay=".4s">
+                            <img src="assets/img/hero/hero_man.png" alt="">
+                        </div>
+                    </div>
+                    <div class="col-xl-5 col-lg-5 col-md-5 col-sm-8">
+                        <div class="hero__caption">
+                            <span data-animation="fadeInRight" data-delay=".4s">Daftarkan Diri Anda</span>
+                            <h1 data-animation="fadeInRight" data-delay=".6s">LAUNDRY <br> KU</h1>
+                            <p data-animation="fadeInRight" data-delay=".8s">Solusi Laundry Praktis Tanpa Keluar Rumah</p>
+                            <!-- Hero-btn -->
+                            <div class="hero__btn" data-animation="fadeInRight" data-delay="1s">
+                                <a href="regirtrasi-pelanggan.php" class="btn hero-btn">DAFTAR</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider Area End-->
+
+    <!-- Category Area Start-->
+    <section class="category-area section-padding30">
+        <div class="container-fluid">
+            <!-- Section Tittle -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="section-tittle text-center mb-85">
+                        <h2>Daftar Laundry</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xl-4 col-lg-6">
+                    <div class="single-category mb-30">
+                        <div class="category-img">
+                            <img src="assets/img/categori/cat1.jpg" alt="">
+                            <div class="category-caption">
+                                <h2>Nadya Laundry</h2>
+                                <p>Alamat : Jl. Diponegoro <br>No 55, Denpasar<br>No. Telp : 0361222</p>
+                                    <span class="best"><a href="#">LIHAT SELENGKAPNYA</a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                     <div class="col-xl-4 col-lg-6">
+                        <div class="single-category mb-30">
+                            <div class="category-img text-center">
+                                <img src="assets/img/categori/cat2.jpg" alt="">
+                                <div class="category-caption">
+                                    <h2>Laundry 2</h2>
+                                    <p>Alamat : Jl. Surabaya <br>No 12, Surabaya<br>No. Telp : 3875120</p>
+                                    <span class="best"><a href="#">LIHAT SELENGKAPNYA</a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-6">
+                        <div class="single-category mb-30">
+                            <div class="category-img text-right">
+                                <img src="assets/img/categori/cat3.jpg" alt="">
+                                <div class="category-caption">
+                                    <h2>Laundry<br>Wina Gans</h2>
+                                    <p>Alamat : Kuta No 22, Badung<br>No. Telp : 57109</p>
+                                    <span class="best"><a href="#">LIHAT SELENGKAPNYA</a></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Category Area End-->
+
+    <!-- menu kalo login -->
+    <div id="body" class>
+        <div class="body">
+            <?php if ( isset($_SESSION["login-pelanggan"]) && isset($_SESSION["pelanggan"]) ) : ?>
                 <ul>
                     <li><button type="button"><a href="pelanggan.php">Profil Saya</a></button></li>
                     <li><button type="button"><a href="status.php">Status Cucian</a></button></li>
                     <li><button type="button"><a href="transaksi.php">Riwayat Transaksi</a></button></li>
                 </ul>
             <?php elseif ( isset($_SESSION["login-agen"]) && isset($_SESSION["agen"]) ) : ?>
-                <div>
-                    <form action="" method="post">
-                        <input type="text" name="keyword" placeholder="Kota / Kabupaten" />
-                        <button type="submit" name="cari">CARI</button>
-                    </form>
-                </div>
                 <ul>
                     <li><button type="button"><a href="agen.php">Profil Saya</a></button></li>
                     <li><button type="button"><a href="status.php">Status Cucian</a></button></li>
                     <li><button type="button"><a href="transaksi.php">Riwayat Transaksi</a></button></li>
                 </ul>
             <?php elseif ( isset($_SESSION["login-admin"]) && isset($_SESSION["admin"]) ) : ?>
-                <div>
-                    <form action="" method="post">
-                        <input type="text" name="keyword" placeholder="Kota / Kabupaten" />
-                        <button type="submit" name="cari">CARI</button>
-                    </form>
-                </div>
                 <ul>
                     <li><button type="button"><a href="admin.php">Profil Saya</a></button></li>
                     <li><button type="button"><a href="status.php">Status Cucian</a></button></li>
                     <li><button type="button"><a href="transaksi.php">Riwayat Transaksi</a></button></li>
                 </ul>
             <?php else : ?>
-                <a href='registrasi-pelanggan.php'><button type='button'>Daftar Sekarang</button></a>
-                <form action="" method="post">
-                    <input type="text" name="keyword" placeholder="Kota / Kabupaten" />
-                    <button type="submit" name="cari">CARI</button>
-                </form>
+                <a href='registrasi-pelanggan.php'><button type='button' class="btn black-btn">Daftar Sekarang</button></a>
             <?php endif ?>
         </div>
-        </div>
-        <div class="body">
-            <div id="halaman">
-                <?php if( $halamanAktif > 1 ) : ?>
-                    <a href="?page=<?= $halamanAktif - 1; ?>">&laquo;</a>
-                <?php endif; ?>
+    </div>
+    <!-- menu akhir kalo login -->
 
-                <?php for( $i = 1; $i <= $jumlahHalaman; $i++ ) : ?>
-                    <?php if( $i == $halamanAktif ) : ?>
-                        <a href="?page=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
-                    <?php else : ?>
-                        <a href="?page=<?= $i; ?>"><?= $i; ?></a>
-                    <?php endif; ?>
-                <?php endfor; ?>
-
-                <?php if( $halamanAktif < $jumlahHalaman ) : ?>
-                    <a href="?page=<?= $halamanAktif + 1; ?>">&raquo;</a>
-                <?php endif; ?>
-            </div>
-            <?php foreach ( $agen as $dataAgen) : ?>
-                <div style="margin-top:20px" class="agen">
-                    <div><a href="detail-agen.php?id=<?= $dataAgen['id_agen'] ?>"><img src="files/laundryku.jpg" width=120 height=120 alt="foto" style="float:left;margin-right:10px"></a></div>
-                    <h3><a href="detail-agen.php?id=<?= $dataAgen['id_agen'] ?>"><?= $dataAgen["nama_laundry"] ?></a></h3>
-                    <?php
-                        $temp = $dataAgen["id_agen"];
-                        $queryStar = mysqli_query($connect,"SELECT * FROM transaksi WHERE id_agen = '$temp'");
-                        $totalStar = 0;
-                        $i = 0;
-                        while ($star = mysqli_fetch_assoc($queryStar)){
-                            $totalStar += $star["rating"];
-                            $i++;
-                            $fixStar = ceil($totalStar / $i);
-                        }
+    <!-- daftar laundry dari database -->
+    <div class="body">      
+        <?php foreach ( $agen as $dataAgen) : ?>
+            <div style="margin-top:20px" class="agen">
+                <div><a href="detail-agen.php?id=<?= $dataAgen['id_agen'] ?>"><img src="files/laundryku.jpg" width=120 height=120 alt="foto" style="float:left;margin-right:10px"></a></div>
+                <h3><a href="detail-agen.php?id=<?= $dataAgen['id_agen'] ?>"><?= $dataAgen["nama_laundry"] ?></a></h3>
+                <?php
+                    $temp = $dataAgen["id_agen"];
+                    $queryStar = mysqli_query($connect,"SELECT * FROM transaksi WHERE id_agen = '$temp'");
+                    $totalStar = 0;
+                    $i = 0;
+                    while ($star = mysqli_fetch_assoc($queryStar)){
+                        $totalStar += $star["rating"];
+                        $i++;
+                        $fixStar = ceil($totalStar / $i);
+                    }
                         
-
-                        if ( $totalStar == 0 ) {
+                    if ( $totalStar == 0 ) {
                     ?>
                         <fieldset class="bintang"><span class="starImg star-0"></span></fieldset>
                         <?php }else { ?>
@@ -147,8 +210,25 @@ if ( isset($_POST["cari"])) {
                         </ul>
                     </div>
                 </div>
-            <?php endforeach; ?>
+        <?php endforeach; ?>
+        <br>
+        <div id="halaman">
+            <?php if( $halamanAktif > 1 ) : ?>
+                <a href="?page=<?= $halamanAktif - 1; ?>">&laquo;</a>
+            <?php endif; ?>
+
+            <?php for( $i = 1; $i <= $jumlahHalaman; $i++ ) : ?>
+                <?php if( $i == $halamanAktif ) : ?>
+                    <a href="?page=<?= $i; ?>" style="font-weight: bold; color: red;"><?= $i; ?></a>
+                <?php else : ?>
+                    <a href="?page=<?= $i; ?>"><?= $i; ?></a>
+                <?php endif; ?>
+            <?php endfor; ?>
+
+            <?php if( $halamanAktif < $jumlahHalaman ) : ?>
+                <a href="?page=<?= $halamanAktif + 1; ?>">&raquo;</a>
+            <?php endif; ?>
         </div>
-    </div>
+    </div>            
 </body>
 </html>
