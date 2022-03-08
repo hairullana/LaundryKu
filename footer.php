72bed7d3724f9dc8130d3f7cd7b024760f666288
Hairul Lana <hairullana99@gmail.com>
2022-03-08 13:13:46 +0800

    change materialize and swal2 to cdn

--- footer.php
+++ footer.php
@@ -40,5 +40,6 @@
 
 <!--  Scripts-->
 <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-<script src="js/materialize.js"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 <!-- end script -->
